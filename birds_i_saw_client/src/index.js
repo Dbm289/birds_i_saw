1a0e76f7f3fb7d2a1ef6a68256359e240b3904ad
Dan <DanielMooreRealPerson@gmail.com>
2021-10-20 20:25:59 -0400

    thunk added to client, some work built out on app.js

--- birds_i_saw_client/src/index.js
+++ birds_i_saw_client/src/index.js
@@ -6,10 +6,13 @@ import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension';
 
+import App from './components/App'
+import { rootReducer } from './reducers/rootReducer';
+
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
-    <Provider store=(store)>
+    <Provider store={store}>
         <App />
 
     </Provider>,
