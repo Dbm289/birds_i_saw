c1129f85f6e75a2f99f4eadb8eb6e143efd0b64e
Dan <DanielMooreRealPerson@gmail.com>
2021-10-31 14:47:01 -0400

    changes to fix gradient navbar for birdsList, adjustments to the birdsList look

--- birds_i_saw_client/src/components/Bird.js
+++ birds_i_saw_client/src/components/Bird.js
@@ -6,13 +6,13 @@ import '../Stylesheet.css'
 const Bird = ({bird, deleteBirdAction, toggleFavAction}) => {
 
     return (
-            <li className="bird">{bird.name} <br /> {bird.last_seen_on} <br /> {bird.location_seen} <br />
+            <ul className="bird">{bird.name} <br /> {bird.last_seen_on} <br /> {bird.location_seen} <br />
                 Number of times seen: {bird.times_seen} <br />
                 Quality of sighting: {bird.quality_of_sightings} <br />
                 Favorite? {bird.favorite ? 'yes' : 'no'} 
                     <input type='checkbox' onChange={(e) => toggleFavAction(e, bird)} name="favorite" defaultChecked={bird.favorite}></input> <br />
                     <input type='button' onClick={(e) => deleteBirdAction(e, bird)} name="delete" value="Delete Bird"></input> 
-            </li>
+            </ul>
     )
 }
 
