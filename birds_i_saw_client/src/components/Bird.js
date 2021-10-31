ba1c087f7a9c7521952fab638d78ec62dfeda08a
Dan <DanielMooreRealPerson@gmail.com>
2021-10-31 14:20:44 -0400

    changes to make bird.js look better, css alignment changes to stop popout menu from covering text

--- birds_i_saw_client/src/components/Bird.js
+++ birds_i_saw_client/src/components/Bird.js
@@ -6,11 +6,11 @@ import '../Stylesheet.css'
 const Bird = ({bird, deleteBirdAction, toggleFavAction}) => {
 
     return (
-            <li className="bird">{bird.name} - {bird.last_seen_on} - {bird.location_seen} 
-                    - Number of times seen: {bird.times_seen} 
-                    - Quality of sighting: {bird.quality_of_sightings} 
-                    - Favorite? {bird.favorite ? 'yes' : 'no'} 
-                    <input type='checkbox' onChange={(e) => toggleFavAction(e, bird)} name="favorite" defaultChecked={bird.favorite}></input>
+            <li className="bird">{bird.name} <br /> {bird.last_seen_on} <br /> {bird.location_seen} <br />
+                Number of times seen: {bird.times_seen} <br />
+                Quality of sighting: {bird.quality_of_sightings} <br />
+                Favorite? {bird.favorite ? 'yes' : 'no'} 
+                    <input type='checkbox' onChange={(e) => toggleFavAction(e, bird)} name="favorite" defaultChecked={bird.favorite}></input> <br />
                     <input type='button' onClick={(e) => deleteBirdAction(e, bird)} name="delete" value="Delete Bird"></input> 
             </li>
     )
